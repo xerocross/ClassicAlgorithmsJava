5fe77631bcc02080b75e4cd3bf0703f348da03f0
xerocross <adam.cross.inc@gmail.com>
2017-03-25 20:09:40 -0400

    Add inner Node class to BST

--- crossutil/BST.java
+++ crossutil/BST.java
@@ -3,5 +3,11 @@ package crossutil;
 // binary search tree
 public class BST<Key extends Comparable<Key>, Value>
 {
-
+	private class Node
+	{
+		public Node left;
+		public Node right;
+		public Value val;
+	}
+	
 }
