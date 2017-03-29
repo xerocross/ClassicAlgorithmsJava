b1a62513571975b7c332d29fd11fbeaf5e3d3b10
xerocross <adam.cross.inc@gmail.com>
2017-03-29 13:48:36 -0400

    Revert "Implement getMedianOf method in Quicksort for getting the median of 3 items.  Add tests to QuicksortTest."
    This reverts commit b4fc46d140f26da34ca8f746015d4cc88a91c5fb.

--- crossutil/QuicksortTest.java
+++ crossutil/QuicksortTest.java
@@ -12,13 +12,5 @@ public class QuicksortTest
 
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
-	
-	@Test
-	public void getMedianOfTest()
-	{
-		assertTrue(Quicksort.getMedianOf(12, 15, 20) == 15);
-		assertTrue(Quicksort.getMedianOf(22, 2, 99) == 22);
-		assertTrue(Quicksort.getMedianOf(5, 25, 7) == 7);
-	}
 
 }
