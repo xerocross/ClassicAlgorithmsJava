eeba092c0eb476ff3467da0a0788b15ba2147282
xerocross <adam.cross.inc@gmail.com>
2017-03-29 14:00:17 -0400

    Implement getPivot in Quicksort and add tests to QuicksortTest.

--- crossutil/QuicksortTest.java
+++ crossutil/QuicksortTest.java
@@ -20,4 +20,56 @@ public class QuicksortTest
 		assertTrue(Quicksort.getMedianOf(22, 2, 99) == 22);
 		assertTrue(Quicksort.getMedianOf(5, 25, 7) == 7);
 	}
+	@Test
+	public void getPivotTest()
+	{
+		List<Integer> list = java.util.Arrays.asList(new Integer[] {26, 16, 81, 53, 54, 68, 11, 98, 20, 45, 7, 33, 29, 13, 50});
+		System.out.println(list);
+		Quicksort.QuickSorter<Integer>  sorter = new QuickSorter<Integer>();
+		sorter.list = list;
+		int size = list.size();
+		Pair<Integer> pivot = sorter.getPivot(1, 6);
+		assertTrue(pivot.value == 53);
+	
+		pivot = sorter.getPivot(0, size);
+		assertTrue(pivot.value == 50);
+	}
+	@Test
+	public void getPivot_outOfBoundsRight() throws RuntimeException
+	{
+		List<Integer> list = java.util.Arrays.asList(new Integer[] {26, 16, 81});
+		System.out.println(list);
+		Quicksort.QuickSorter<Integer>  sorter = new QuickSorter<Integer>();
+		sorter.list = list;
+		assertTrue(sorter.getPivot(0,3).value == 26);
+		thrown.expect(RuntimeException.class);
+		sorter.getPivot(0,4);
+	}
+	
+	@Test
+	public void getPivot_outOfBoundsLeft() throws RuntimeException
+	{
+		List<Integer> list = java.util.Arrays.asList(new Integer[] {26, 16, 81});
+		System.out.println(list);
+		Quicksort.QuickSorter<Integer>  sorter = new QuickSorter<Integer>();
+		sorter.list = list;
+		thrown.expect(RuntimeException.class);
+		sorter.getPivot(-1,2);
+	}
+
+	@Test
+	public void getPivot_short()
+	{
+		List<Integer> list = java.util.Arrays.asList(new Integer[] {26, 16});
+		System.out.println(list);
+		Quicksort.QuickSorter<Integer>  sorter = new QuickSorter<Integer>();
+		sorter.list = list;
+		assertTrue(sorter.getPivot(0,2).value == 26);
+		
+		list = java.util.Arrays.asList(new Integer[] {26});
+		System.out.println(list);
+		sorter = new QuickSorter<Integer>();
+		sorter.list = list;
+		assertTrue(sorter.getPivot(0,1).value == 26);
+	}
 }
