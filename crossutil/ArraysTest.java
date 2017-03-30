980ccdb36ba0210b3e83827021bf7209fe6826c1
xerocross <adam.cross.inc@gmail.com>
2017-03-30 01:09:43 -0400

    Clean up Arrays.  Remove methods that should be encapsulated elsewhere.  Start building associated tests in ArraysTest.java

--- crossutil/ArraysTest.java
+++ crossutil/ArraysTest.java
@@ -3,12 +3,27 @@ package crossutil;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import java.util.*;
+
 
 public class ArraysTest 
 {
-	@Test
-	public void test()
+	private boolean isPermutation(Integer[] a)
 	{
-		assertTrue(false);
+		int len = a.length;
+		List<Integer> list = Arrays.asList(a);
+		for (int i = 0; i < len; i++)
+			if (list.contains(new Integer(i)) == false)
+				return false;
+		return true;
+	}
+	
+	@Test
+	public void permutationTest_isPermutation()
+	{
+		Integer[] p = Arrays.permutation(20);
+		System.out.println(Arrays.asList(p));
+		boolean isPermutation = isPermutation(p);
+		assertTrue(isPermutation);
 	}
 }
