ec30ec61eb6ad40af0f50f145c9949cc2fa34630
xerocross <adam.cross.inc@gmail.com>
2017-04-05 20:12:14 -0400

    implement balance method in RedBlackTree and testing

--- crossutil/RedBlackTree.java
+++ crossutil/RedBlackTree.java
@@ -1,44 +1,20 @@
 package crossutil;
 
-import crossutil.BST.Node;
 import crossutil.BST.Relation;
 
 public class RedBlackTree <Key extends Comparable<Key>, Value>
 {
-
-	class Node implements Comparable<Node>
+	Node root;
+	
+	
+	public void put(Key key, Value val)
 	{
-		Node left;
-		Node right;
-		Node parent;
-		public Key key;
-		public Value value;
-		public int size; // cardinality of subtree rooted at node
-		public Node(Key key, Value value)
-		{
-			this.size = 1;
-			this.key = key;
-			this.value = value;
-		}
-		@Override
-		public String toString()
-		{
-			return key == null ? "null" : key.toString();
-		}
-		public int compareTo(Node o)
-		{
-			return this.key.compareTo(o.key);
-		}
-		public Relation compare(Node o)
-		{
-			int cmp = this.compareTo(o);
-			return (cmp == 0 ? Relation.EQUAL : ((cmp > 0) ? Relation.GREATER : Relation.LESS));
-		}
-		public Relation compare(Key key)
-		{
-			int cmp = this.key.compareTo(key);
-			return (cmp == 0 ? Relation.EQUAL : ((cmp > 0) ? Relation.GREATER : Relation.LESS));
-		}
+		root = put(root, key, val);
+	}
+	public Value get(Key key)
+	{
+		Node result = get(root, key);
+		return (result == null) ? null : result.value;
 	}
 	Node put(Node node, Key key, Value value)
 	{
@@ -75,8 +51,7 @@ public class RedBlackTree <Key extends Comparable<Key>, Value>
 	{
 		if (node == null)
 			return null;
-		Relation rel = getRelation(node.key, key);
-		switch(rel)
+		switch(getRelation(node.key, key))
 		{
 		case LESS:
 			return get(node.right, key);
@@ -88,4 +63,103 @@ public class RedBlackTree <Key extends Comparable<Key>, Value>
 			throw new RuntimeException();
 		}
 	}
+	
+	void attachLeft(Node parent, Node child)
+	{
+		parent.left = child;
+		child.parent = parent;
+	}
+	void attachRight(Node parent, Node child)
+	{
+		parent.right = child;
+		child.parent = parent;
+	}
+	Node rotateLeft(Node node)
+	{
+		Node newTop = node.right;
+		Node gamma = newTop.left;
+		boolean topIsRed = node.red;
+		boolean rightLinkIsRed = node.right.red;
+		attachLeft(newTop, node);
+		//newTop.left = node;
+		if (exists(gamma))
+			attachRight(node, gamma);
+		//node.right = gamma;
+		newTop.red = topIsRed;
+		newTop.left.red = rightLinkIsRed;
+		return newTop;
+	}
+	Node rotateRight(Node node)
+	{
+		Node newTop = node.left;
+		Node gamma = newTop.right;
+		boolean topIsRed = node.red;
+		boolean rightLinkIsRed = node.left.red;
+		attachRight(newTop, node);
+		//newTop.right = node;
+		if (exists(gamma))
+			attachLeft(node, gamma);
+		//node.left = gamma;
+		newTop.red = topIsRed;
+		newTop.right.red = rightLinkIsRed;
+		return newTop;
+	}
+	boolean exists(Node node)
+	{
+		return (node != null);
+	}
+	void flipColor(Node node)
+	{
+		node.left.red = false;
+		node.right.red = false;
+		node.red = true;
+	}
+	Node balance(Node node)
+	{
+		if (exists(node.left) && node.left.red && exists(node.left.right) && node.left.right.red)
+			node.left = rotateLeft(node.left);
+		if (exists(node.left) && node.left.red && exists(node.left.left) && node.left.left.red)
+			node = rotateRight(node);
+		if (exists(node.left) && node.left.red && exists(node.right) && node.right.red)
+			flipColor(node);
+		return node;
+	}
+	class Node implements Comparable<Node>
+	{
+		Node left;
+		Node right;
+		Node parent;
+		boolean red;
+		public Key key;
+		public Value value;
+		public int size; // cardinality of subtree rooted at node
+		public Node(){}
+		public Node(Key key, Value value)
+		{
+			this.size = 1;
+			this.key = key;
+			this.value = value;
+			this.red = true;
+		}
+		@Override
+		public String toString()
+		{
+			return String.format("(%s,%s)", (key == null ? "null" : key.toString()), (value == null ? "null" : value.toString()));
+		}
+		public int compareTo(Node o)
+		{
+			return this.key.compareTo(o.key);
+		}
+		public Relation compare(Node o)
+		{
+			int cmp = this.compareTo(o);
+			return (cmp == 0 ? Relation.EQUAL : ((cmp > 0) ? Relation.GREATER : Relation.LESS));
+		}
+		public Relation compare(Key key)
+		{
+			int cmp = this.key.compareTo(key);
+			return (cmp == 0 ? Relation.EQUAL : ((cmp > 0) ? Relation.GREATER : Relation.LESS));
+		}
+	}
+
 }
--- crossutil/RedBlackTreeTest.java
+++ crossutil/RedBlackTreeTest.java
@@ -7,8 +7,179 @@ import org.junit.Test;
 public class RedBlackTreeTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void putTest() 
+	{
+		RedBlackTree<Integer, Integer> rbt = new RedBlackTree<>();
+		rbt.put(3,3);
+		assertTrue(rbt.root.value == 3);
 	}
-
+	@Test
+	public void putColorTest() 
+	{
+		RedBlackTree<Integer, Integer> rbt = new RedBlackTree<>();
+		rbt.put(3,3);
+		assertTrue(rbt.root.red);
+	}
+	@Test
+	public void getTest() 
+	{
+		RedBlackTree<Integer, Integer> rbt = new RedBlackTree<>();
+		rbt.put(3,3);
+		rbt.put(2,2);
+		rbt.put(4,4);
+		rbt.put(7,7);
+		assertTrue(rbt.get(7).equals(7));
+	}
+	@Test
+	public void attachLeftTest() 
+	{
+		RedBlackTree<Integer, Integer>.Node apple, banana;
+		RedBlackTree<Integer, Integer> rbt = new RedBlackTree<>();
+		apple = rbt.new Node();
+		banana = rbt.new Node();
+		rbt.attachLeft(banana, apple);
+		assertTrue(banana.left == apple && apple.parent == banana);
+	}
+	@Test
+	public void attachRightTest() 
+	{
+		RedBlackTree<Integer, Integer>.Node apple, banana;
+		RedBlackTree<Integer, Integer> rbt = new RedBlackTree<>();
+		apple = rbt.new Node();
+		banana = rbt.new Node();
+		rbt.attachRight(banana, apple);
+		assertTrue(banana.right == apple && apple.parent == banana);
+	}
+	@Test
+	public void rotateLeftTest_form()
+	{
+		RedBlackTree<Integer, Integer>.Node apple, banana, cherry, result;
+		RedBlackTree<Integer, Integer> rbt = new RedBlackTree<>();
+		apple = rbt.new Node();
+		banana = rbt.new Node();
+		cherry = rbt.new Node();
+		rbt.attachRight(apple, cherry);
+		rbt.attachLeft(cherry, banana);
+		result = rbt.rotateLeft(apple);
+		assertTrue(result == cherry && result.left == apple && apple.parent == result && result.left.right == banana && banana.parent == result.left);
+	}
+	@Test
+	public void rotateLeftTest_color()
+	{
+		RedBlackTree<Integer, Integer>.Node apple, banana, cherry, result;
+		RedBlackTree<Integer, Integer> rbt = new RedBlackTree<>();
+		
+		for(int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				apple = rbt.new Node();
+				banana = rbt.new Node();
+				cherry = rbt.new Node();
+				rbt.attachRight(apple, cherry);
+				rbt.attachLeft(cherry, banana);
+				apple.red = (i % 2 == 0);
+				cherry.red = (j % 2 == 0);
+				boolean isRed1 = apple.red;
+				boolean isRed2 = cherry.red;
+				result = rbt.rotateLeft(apple);
+				assertTrue(result.red == isRed1 && result.left.red == isRed2);
+			}
+		}
+	}
+	@Test
+	public void rotateRightTest_form()
+	{
+		RedBlackTree<Integer, Integer>.Node apple, banana, cherry, result;
+		RedBlackTree<Integer, Integer> rbt = new RedBlackTree<>();
+		apple = rbt.new Node();
+		banana = rbt.new Node();
+		cherry = rbt.new Node();
+		rbt.attachLeft(cherry, apple);
+		rbt.attachRight(apple, banana);
+		result = rbt.rotateRight(cherry);
+		assertTrue(result == apple && result.right == cherry && cherry.parent == result && result.right.left == banana && banana.parent == result.right);
+	}
+	@Test
+	public void rotateRightTest_color()
+	{
+		RedBlackTree<Integer, Integer>.Node apple, banana, cherry, result;
+		RedBlackTree<Integer, Integer> rbt = new RedBlackTree<>();
+		
+		for(int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				apple = rbt.new Node();
+				banana = rbt.new Node();
+				cherry = rbt.new Node();
+				rbt.attachLeft(cherry, apple);
+				rbt.attachRight(apple, banana);
+				apple.red = (i % 2 == 0);
+				cherry.red = (j % 2 == 0);
+				boolean isRed1 = cherry.red;
+				boolean isRed2 = apple.red;
+				result = rbt.rotateRight(cherry);
+				assertTrue(result.red == isRed1 && result.right.red == isRed2);
+			}
+		}
+	}
+	@Test
+	public void insertInto3NodeTest_left()
+	{
+		//build 3-node
+		RedBlackTree<Integer, Integer>.Node apple, banana, cherry, result;
+		RedBlackTree<Integer, Integer> rbt = new RedBlackTree<>();
+		apple = rbt.new Node(10,10);
+		banana = rbt.new Node(8,8);
+		banana.red = true;
+		rbt.root = apple;
+		rbt.attachLeft(apple, banana);
+		//attach new node at left
+		cherry = rbt.new Node(7,7);
+		rbt.attachLeft(banana, cherry);
+		cherry.red = true;
+		//balance
+		result = rbt.balance(apple);
+		assertTrue(result == banana && result.left == cherry && result.right == apple);
+	}
+	@Test
+	public void insertInto3NodeTest_middle()
+	{
+		//build 3-node
+		RedBlackTree<Integer, Integer>.Node apple, banana, cherry, result;
+		RedBlackTree<Integer, Integer> rbt = new RedBlackTree<>();
+		apple = rbt.new Node(10,10);
+		banana = rbt.new Node(8,8);
+		banana.red = true;
+		rbt.root = apple;
+		rbt.attachLeft(apple, banana);
+		//attach new node at middle
+		cherry = rbt.new Node(9,9);
+		rbt.attachRight(banana, cherry);
+		cherry.red = true;
+		//balance
+		result = rbt.balance(apple);
+		assertTrue(result == cherry && cherry.left == banana && cherry.right == apple);
+	}
+	@Test
+	public void insertInto3NodeTest_right()
+	{
+		//build 3-node
+		RedBlackTree<Integer, Integer>.Node apple, banana, cherry, result;
+		RedBlackTree<Integer, Integer> rbt = new RedBlackTree<>();
+		apple = rbt.new Node(10,10);
+		banana = rbt.new Node(8,8);
+		banana.red = true;
+		rbt.root = apple;
+		rbt.attachLeft(apple, banana);
+		//attach new node at right
+		cherry = rbt.new Node(11,11);
+		rbt.attachRight(apple, cherry);
+		cherry.red = true;
+		// balance
+		result = rbt.balance(apple);
+		assertTrue(result == apple && !banana.red && !cherry.red && apple.red);
+	}
+	
 }
