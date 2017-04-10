4fe9e2d72a27e8769daa756e1fc6fd317eca0cea
xerocross <adam.cross.inc@gmail.com>
2017-04-10 03:14:35 -0400

    minor cleaning

--- crossutil/Sorter.java
+++ crossutil/Sorter.java
@@ -1,9 +1,7 @@
 package crossutil;
 import java.util.List;
 
-
 public interface Sorter 
 {
 	public <U extends Comparable<U>> List<U> sort(List<U> list);
-	public <U extends Comparable<U>> U[] sort(U[] list); 
 }
