a7b16d0e96fcad34c353d403def93a1f707e910d
xerocross <adam.cross.inc@gmail.com>
2017-04-10 02:44:25 -0400

    Add a test for MergeSort2

--- crossutil/MergeSortTest.java
+++ crossutil/MergeSortTest.java
@@ -5,9 +5,8 @@ import java.util.List;
 import org.junit.Test;
 
 public class MergeSortTest {
-
 	@Test
-	public void sortTestforList()
+	public void mergesortTest()
 	{
 		int numRounds = 40;
 		for (int i = 0; i < numRounds; i++)
@@ -15,6 +14,21 @@ public class MergeSortTest {
 			List<Integer> list = Arrays.randomIntegerArrayList(10+3*i, 100+20*i);
 			System.out.println(list);
 			List<Integer> sortedList = MergeSort.sort(list);
+			System.out.println(sortedList);
+			System.out.println("***");
+			boolean isSorted = Arrays.isSorted(sortedList);
+			assertTrue(isSorted);
+		}
+	}
+	@Test
+	public void mergesort2Test()
+	{
+		int numRounds = 40;
+		for (int i = 0; i < numRounds; i++)
+		{
+			List<Integer> list = Arrays.randomIntegerArrayList(10+3*i, 100+20*i);
+			System.out.println(list);
+			List<Integer> sortedList = MergeSort2.sort(list);
 			System.out.println(sortedList);
 			System.out.println("***");
 			boolean isSorted = Arrays.isSorted(sortedList);
